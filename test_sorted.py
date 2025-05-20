f4efd742b53faca1878bf6ea6c45ec57d4502eec
Tom Hofmann <tom@scheidweg.net>
2025-05-20 08:06:11 +0200

    drop 2M stuff

--- test_sorted.py
+++
@@ -1,9 +0,0 @@
-from sorting import *
-
-def test_sorted():
-    assert is_sorted(['Apfel', 'Birne', 'Zwetschge'])
-    assert not is_sorted(['Birne', 'Apfel', 'Zwetschge'])
-
-
-def test_sorted_bug():
-    assert not is_sorted(['Apfel', 'Zwetschge', 'Birne'])
